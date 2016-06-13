6c89abbfe23238d795dda898a7593aca4a0e24d4
masterUNG <sukuma99@gmail.com>
2016-06-13 11:42:19 +0700

    Intent

--- app/src/main/java/pbru/it/sukuma/pbrurun/MainActivity.java
+++ app/src/main/java/pbru/it/sukuma/pbrurun/MainActivity.java
@@ -1,13 +1,21 @@
 package pbru.it.sukuma.pbrurun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }   //Main Method
+
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
     }
-}
+
+}   //Main Class
